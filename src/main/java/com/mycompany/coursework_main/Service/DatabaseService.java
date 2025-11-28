6baea5cb83e511ab56a8d36a68fc6aee7935239f
N1209868 <dmytro.hershchuk2023@my.ntu.ac.uk>
2025-11-28 11:07:00 +0000

    Organise connection with database, added function for fetching items

--- src/main/java/com/mycompany/coursework_main/Service/DatabaseService.java
+++ src/main/java/com/mycompany/coursework_main/Service/DatabaseService.java
@@ -3,11 +3,74 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.coursework_main.Service;
-
+import com.azure.cosmos.*;
+import com.azure.cosmos.models.CosmosQueryRequestOptions;
+import com.azure.cosmos.models.SqlParameter;
+import com.azure.cosmos.models.SqlQuerySpec;
+import com.azure.cosmos.util.CosmosPagedIterable;
+import com.mycompany.coursework_main.Model.Items;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Sergii
  */
 public class DatabaseService {
+    private static final String HOST = "https://myfirstfreedb.documents.azure.com:443/";
+    private static final String KEY = "";
+    private static final String DATABASE_NAME = "coursework";
     
+    private CosmosClient client;
+    private CosmosContainer itemContainer;    
+    //private CosmosContainer requestContainer;
+
+    //Create connection to CosmosDb
+    public DatabaseService() {
+        this.client = new CosmosClientBuilder()
+            .endpoint(HOST)
+            .key(KEY)
+            .consistencyLevel(ConsistencyLevel.EVENTUAL)
+            .buildClient();
+
+        CosmosDatabase database = client.getDatabase(DATABASE_NAME);
+        this.itemContainer = database.getContainer("items");
+        //this.requestContainer = database.getContainer("requests");
+    }
+
+    //Working with items requests
+    public List<Items> getItemsFiltered(String city, Double maxPrice) {
+        List<Items> resultList = new ArrayList<>();
+        List<SqlParameter> params = new ArrayList<>();
+        
+        StringBuilder sql = new StringBuilder("Select * FROM c WHERE 1=1");
+        
+        //Fetching imems with only city parameter
+        if(city != null && !city.isEmpty()){
+            sql.append(" AND LOWER(c.location) = @city");
+            params.add(new SqlParameter("@city", city.toLowerCase()));
+        }
+        
+        //Fetching imems with only price parameter
+        if(maxPrice != null && maxPrice > 0){
+            sql.append(" AND c.daily_rate <= @maxPrice");
+            params.add(new SqlParameter("@maxPrice", maxPrice));
+        }
+        
+        SqlQuerySpec querySpec = new SqlQuerySpec(sql.toString(), params);
+        try {
+            CosmosPagedIterable<Items> response = itemContainer.queryItems(
+                querySpec,
+                new CosmosQueryRequestOptions(),
+                Items.class
+            );
+
+            response.forEach(resultList::add);
+            
+        } catch (Exception e) {
+            System.err.println("Searching error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        return resultList;
+    }
 }
