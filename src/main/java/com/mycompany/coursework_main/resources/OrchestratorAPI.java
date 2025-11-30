7fd7297289361ce829b98f73ba9656d5ed406532
N1209868 <dmytro.hershchuk2023@my.ntu.ac.uk>
2025-11-30 11:34:15 +0000

    Update searchItems method to calculating distance between item and user

--- src/main/java/com/mycompany/coursework_main/resources/OrchestratorAPI.java
+++ src/main/java/com/mycompany/coursework_main/resources/OrchestratorAPI.java
@@ -2,6 +2,8 @@ package com.mycompany.coursework_main.resources;
 
 import com.mycompany.coursework_main.Model.Items;
 import com.mycompany.coursework_main.Service.DatabaseService;
+import com.mycompany.coursework_main.Service.DistanceService;
+import java.io.IOException;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,6 +20,7 @@ import javax.ws.rs.core.Response;
 public class OrchestratorAPI {
     
     private DatabaseService dbService = new DatabaseService();
+    private DistanceService distanceService = new DistanceService();
     
     @GET
     @Path("/ping")
@@ -30,8 +33,21 @@ public class OrchestratorAPI {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Items> searchItems(
         @QueryParam("city") String city,
-        @QueryParam("maxPrice") Double maxPrice) {
-    return dbService.getItemsFiltered(city, maxPrice);
-}
+        @QueryParam("maxPrice") Double maxPrice,
+        @QueryParam("userLat") Double userLat,
+        @QueryParam("userLon") Double userLon) throws IOException, InterruptedException {
+            
+        List<Items> items = dbService.getItemsFiltered(city, maxPrice);
+        if(userLat != null && userLon != null){
+            for(Items item : items){
+                if(item.getLatitude() != null && item.getLongitude() != null){
+                    Double dist = distanceService.getDistance(userLon, userLat, item.getLatitude(), item.getLongitude());
+                    item.setDistance(dist);
+                }
+            }
+        }
+        return items;
+    }
+    
 }
 
