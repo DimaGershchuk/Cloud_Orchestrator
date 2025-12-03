368eb835df02c4f86954dc890294523451a6d2b6
N1209868 <dmytro.hershchuk2023@my.ntu.ac.uk>
2025-12-03 11:48:40 +0000

    Created PUT method for changing request`s status

--- src/main/java/com/mycompany/coursework_main/resources/OrchestratorAPI.java
+++ src/main/java/com/mycompany/coursework_main/resources/OrchestratorAPI.java
@@ -9,7 +9,9 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -60,10 +62,33 @@ public class OrchestratorAPI {
         request.setStatus("pending");
         try {
             dbService.createRequest(request);
-            return Response.status(Response.Status.CREATED).entity(request).build();
+            return Response.status(Response.Status.CREATED).entity(request).build(); // Better control over what happens in case of error
         } catch (Exception e) {
             return Response.serverError().entity("Error creating request").build();
         }
+    }
+    
+    @PUT
+    @Path("/requests/{id}/cancel")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response cancelRequest(@PathParam("id") String id){
+        
+       RentalRequest request = dbService.getRequestById(id); //Searching for specific item
+       
+       if(request == null){
+           return Response.status(Response.Status.NOT_FOUND).entity("Order not found").build();
+       }
+       
+       request.setStatus("cancelled"); //Changing request status after cancel it
+       
+       //Saving changes
+       try {
+           dbService.updateRequest(request);
+           return Response.ok(request).build();
+       } catch (Exception e) {
+           return Response.serverError().entity("Error updating status").build();
+       }
+       
     }
 }
 
